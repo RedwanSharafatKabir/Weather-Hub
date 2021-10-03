c5d7562a60186f4a785da00c7bd36fb7b9e4fce2
Redwan Sharafat Kabir <37416018+RedwanSharafatKabir@users.noreply.github.com>
2021-10-03 17:21:15 +0600

    Weather Forecast work started

---
+++ WeatherReader/app/src/main/java/com/example/weatherreader/ModelClasses/WeatherForecastResult.java
@@ -0,0 +1,5 @@
+package com.example.weatherreader.ModelClasses;
+
+public class WeatherForecastResult {
+
+}
--- WeatherReader/app/src/main/java/com/example/weatherreader/Retrofit/RetrofitInterface.java
+++ WeatherReader/app/src/main/java/com/example/weatherreader/Retrofit/RetrofitInterface.java
@@ -1,5 +1,6 @@
 package com.example.weatherreader.Retrofit;
 
+import com.example.weatherreader.ModelClasses.WeatherForecastResult;
 import com.example.weatherreader.ModelClasses.WeatherResult;
 
 import io.reactivex.Observable;
@@ -8,13 +9,16 @@ import retrofit2.http.Query;
 
 public interface RetrofitInterface {
 
-//    @GET("weather?id=1337178&appid=372c08b8e0c21f52d218a9699b8da4c7")
-//    Call<Example> getWeatherData(@Query("q") String name);
-
     @GET("weather")
     Observable<WeatherResult> getWeatherLatLng(@Query("lat") String lat,
                                                @Query("lon") String lon,
                                                @Query("appid") String appid,
                                                @Query("units") String unit);
+
+//    @GET("forecast")
+//    Observable<WeatherForecastResult> getWeatherLatLng(@Query("lat") String lat,
+//                                                       @Query("lon") String lon,
+//                                                       @Query("appid") String appid,
+//                                                       @Query("units") String unit);
 
 }
