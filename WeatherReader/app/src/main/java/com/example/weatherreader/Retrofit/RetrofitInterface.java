9889016d574d2acac21af9acbfd9e2895999cbab
Redwan Sharafat Kabir <37416018+RedwanSharafatKabir@users.noreply.github.com>
2021-10-19 17:34:17 +0600

    API Client added

--- WeatherReader/app/src/main/java/com/example/weatherreader/Retrofit/RetrofitInterface.java
+++ WeatherReader/app/src/main/java/com/example/weatherreader/Retrofit/RetrofitInterface.java
@@ -2,7 +2,6 @@ package com.example.weatherreader.Retrofit;
 
 import com.example.weatherreader.ModelClasses.WeatherForecastResult;
 import com.example.weatherreader.ModelClasses.WeatherResult;
-
 import io.reactivex.Observable;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -15,10 +14,10 @@ public interface RetrofitInterface {
                                                @Query("appid") String appid,
                                                @Query("units") String unit);
 
-//    @GET("forecast")
-//    Observable<WeatherForecastResult> getWeatherLatLng(@Query("lat") String lat,
-//                                                       @Query("lon") String lon,
-//                                                       @Query("appid") String appid,
-//                                                       @Query("units") String unit);
+    @GET("forecast")
+    Observable<WeatherForecastResult> getForeCastWeatherLatLng(@Query("lat") String lat,
+                                                       @Query("lon") String lon,
+                                                       @Query("appid") String appid,
+                                                       @Query("units") String unit);
 
 }
